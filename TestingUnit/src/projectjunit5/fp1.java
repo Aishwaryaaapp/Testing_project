c3bfb2d07783c8b0d93e767e852f4e67a8ed32c1
Aishwarya N <Aishwarya N@DESKTOP-5VV1H3A>
2022-07-20 22:06:44 +0530

    first commit

---
+++ TestingUnit/src/projectjunit5/fp1.java
@@ -0,0 +1,26 @@
+package projectjunit5;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class fp1 {
+	
+	@Test
+	void disp()
+	{
+		System.out.println("hello");
+	}
+	@Disabled
+	void disp1()
+	{
+		System.out.println("Hi");
+	}
+	@Test
+	void a3()
+	{
+		System.out.println("jyothi");
+	}
+
+	}
+	
+
+
